f5906df34c400bae3d2b193b2982b7cf577b8ccf
Sasikanth Miriyampalli <hello@sasikanth.dev>
2023-08-08 10:01:36 +0530

    Simplify login flow benchmark

--- benchmark/src/main/java/org/dhis2/benchmark/LoginFlowBenchmark.kt
+++ benchmark/src/main/java/org/dhis2/benchmark/LoginFlowBenchmark.kt
@@ -1,14 +1,13 @@
 package org.dhis2.benchmark
 
-import android.app.ActivityManager
-import android.content.Context.ACTIVITY_SERVICE
+import android.content.Intent
+import androidx.benchmark.macro.CompilationMode
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
-import androidx.test.uiautomator.Direction
 import androidx.test.uiautomator.Until
 import org.junit.Rule
 import org.junit.Test
@@ -28,9 +27,9 @@ class LoginFlowBenchmark {
     @Test
     fun login() = benchmarkRule.measureRepeated(
         packageName = "com.dhis2",
-        // TODO: Use trace section metric instead of startup?
         metrics = listOf(StartupTimingMetric()),
         iterations = 11,
+        compilationMode = CompilationMode.DEFAULT,
         startupMode = StartupMode.COLD,
         setupBlock = {
             instrumentation.uiAutomation
@@ -38,8 +37,7 @@ class LoginFlowBenchmark {
                 .close()
         }
     ) {
-        pressHome()
-        startActivityAndWait()
+        startActivityAndWait(Intent("$packageName.LOGIN_ACTIVITY"))
 
         // Credentials Screen
         device.wait(
@@ -47,34 +45,11 @@ class LoginFlowBenchmark {
             TimeUnit.SECONDS.toMillis(10)
         )
 
-        device.findObject(
-            By.res(packageName, "server_url_edit")
-        ).text = "https://play.dhis2.org/40"
+        device.findObject(By.res(packageName, "server_url_edit")).text = "https://play.dhis2.org/40"
+        device.findObject(By.res(packageName, "user_name_edit")).text = "android"
+        device.findObject(By.res(packageName, "user_pass_edit")).text = "Android123"
+        device.findObject(By.res(packageName, "login")).click()
 
-        device.findObject(
-            By.res(packageName, "user_name_edit")
-        ).text = "android"
-
-        device.findObject(
-            By.res(packageName, "user_pass_edit")
-        ).text = "Android123"
-
-        device.findObject(
-            By.res(packageName, "login")
-        ).click()
-
-        // Opt-in for analytics
-        device.wait(
-            Until.hasObject(By.text("Do you want to help us improve this app?")),
-            TimeUnit.SECONDS.toMillis(10)
-        )
-
-        device.findObject(By.text("Yes")).click()
-
-        // Home
-        device.wait(
-            Until.hasObject(By.text("Home")),
-            TimeUnit.SECONDS.toMillis(60)
-        )
+        device.waitForIdle()
     }
 }
