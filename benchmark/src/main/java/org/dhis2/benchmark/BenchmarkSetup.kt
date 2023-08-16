34aff28c26900b70375eb57a8f106001dcb7f705
Siddharth Agarwal <sagarwal@rtsl.org>
2023-08-17 02:46:30 +0530

    Add benchmark rule class

---
+++ benchmark/src/main/java/org/dhis2/benchmark/BenchmarkSetup.kt
@@ -0,0 +1,26 @@
+package org.dhis2.benchmark
+
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.FrameTimingMetric
+import androidx.benchmark.macro.MacrobenchmarkScope
+import androidx.benchmark.macro.Metric
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+
+fun MacrobenchmarkRule.setRule(
+  metrics: List<Metric> = listOf(StartupTimingMetric(), FrameTimingMetric()),
+  setupBlock: MacrobenchmarkScope.() -> Unit = {},
+  measureBlock: MacrobenchmarkScope.() -> Unit = {}
+) {
+  measureRepeated(
+    packageName = TARGET_PACKAGE,
+    metrics = metrics,
+    compilationMode = CompilationMode.DEFAULT,
+    startupMode = StartupMode.COLD,
+    iterations = DEFAULT_ITERATIONS,
+    setupBlock = setupBlock,
+  ) {
+    measureBlock()
+  }
+}
