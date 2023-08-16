84356685c9ab46cecf6a1f8ec10a5611fa580529
Siddharth Agarwal <sagarwal@rtsl.org>
2023-08-17 02:45:57 +0530

    Add constant class

---
+++ benchmark/src/main/java/org/dhis2/benchmark/Constants.kt
@@ -0,0 +1,12 @@
+package org.dhis2.benchmark
+
+const val TARGET_PACKAGE = "com.dhis2"
+const val DEFAULT_ITERATIONS = 1
+
+//Login
+const val SERVER_URL = "https://dhis2-sandbox2.simple.org"
+const val USERNAME = "admin"
+const val PASSWORD = "district"
+
+//CVH
+const val HTN_PROGRAM = "Hypertension & Diabetes"
